4644b342d52be8f96a7fedb61b9cace63c2962b7
Josefina Medica <medicajosefina@gmail.com>
2023-09-28 21:14:18 -0300

    entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import  NavBar  from './components/NavBar';
 import  ItemListContainer  from './components/ItemListContainer';
-import  CartWidget  from './components/CartWidget';
+
 
 function App() {
-  const cartItemCount = 1;
   return (
     <>
       <NavBar/>
-      <CartWidget itemCount={cartItemCount} />
-      <ItemListContainer greeting="Hola, somos La Provence" />
+      <ItemListContainer greeting={"Hola, somos La Provence"}/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
